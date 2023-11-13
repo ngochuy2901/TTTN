780678a3bfdd17b79f3446c76ba667f52899d0e9
Ngoc Huy <daongochuy06@gmail.com>
2023-11-13 13:06:47 +0700

    13/11/2023

--- JavaProject/Library/src/main/java/com/store/library/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.store.library.service;
-
-import com.store.library.dto.AdminDto;
-import com.store.library.model.Admin;
-
-public interface AdminService {
-    Admin findByUsername(String username);
-    Admin save(AdminDto saveAdminDto);
-}
